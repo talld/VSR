7d7301ceb84e743af35191e1e72e6916091994d9
talldie <ew19605@essex.ac.uk>
2022-10-13 11:44:35 +0100

    Added: tests files for testing library code.

--- Tests/tests.c
+++ Tests/tests.c
@@ -1,4 +1,12 @@
+#include <VSR.h>
+
 int main(int argc, char* argv[])
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("TestWindow", 0,0,640,480,
+								   SDL_WINDOW_VULKAN);
+	
+	VSR_RendererCreateInfo* createInfo = VSR_RendererGenerateCreateInfo(window,0L);
+	VSR_CreateRenderer(createInfo);
 	return 0;
 }
