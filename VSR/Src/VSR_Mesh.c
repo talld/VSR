0aa823cc4f5db3ed9516a25035f8c9f69b376244
talldie <windyvagina@gmail.com>
2022-12-06 02:31:47 +0000

    Added: mesh Meshes are the host local data that gets converted into the device local models

---
+++ VSR/Src/VSR_Mesh.c
@@ -0,0 +1,65 @@
+#include "VSR_Mesh.h"
+
+#include <SDL2/SDL.h>
+
+#include "VSR_error.h"
+
+
+
+
+
+//==============================================================================
+// VSR_MeshCreate
+//------------------------------------------------------------------------------
+VSR_Mesh*
+VSR_MeshCreate(
+	VSR_Vertex* vertices,
+	size_t vertexCount,
+	uint32_t* indices,
+	size_t indexCount)
+{
+	VSR_Mesh* mesh;
+	mesh = SDL_malloc(sizeof(VSR_Mesh));
+
+	size_t vertSize = vertexCount * sizeof(VSR_Vertex);
+	mesh->vertices = malloc(vertSize);
+	mesh->vertexCount = vertexCount;
+	memcpy(mesh->vertices, vertices, vertSize);
+
+
+	if(indices)
+	{
+		size_t indicesSize = indexCount * sizeof(VSR_Index);
+		mesh->indices    = malloc(indicesSize);
+		mesh->indexCount = indexCount;
+		memcpy(mesh->indices, indices, indicesSize);
+	}
+
+	return mesh;
+}
+
+
+
+
+
+//==============================================================================
+// VSR_MeshFree
+//------------------------------------------------------------------------------
+void
+VSR_MeshFree(
+	VSR_Mesh* mesh)
+{
+	SDL_free(mesh->vertices);
+
+	if(mesh->indices)
+	{
+		SDL_free(mesh->indices);
+	}
+
+	if(mesh->UVs)
+	{
+		SDL_free(mesh->UVs);
+	}
+
+	SDL_free(mesh);
+}
---
+++ VSR/Src/VSR_Mesh.h
@@ -0,0 +1,20 @@
+#ifndef VSR_MODEL_H
+#define VSR_MODEL_H
+
+VSR_Mesh*
+VSR_MeshCreate(
+	VSR_Vertex* vertices,
+	size_t vertexCount,
+	uint32_t* indices,
+	size_t indexCount);
+
+void
+VSR_MeshFree(
+	VSR_Mesh* model);
+
+int
+VSR_MeshBindUVs(
+	VSR_Mesh* model,
+	float* UVs);
+
+#endif //VSR_MODEL_H
